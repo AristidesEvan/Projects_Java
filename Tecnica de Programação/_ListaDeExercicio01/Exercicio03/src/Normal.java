9e021ca4a5c2ca028e4ec80fc3ae12eccc519582
Aristides Evandro <aristidesevandro@gmail.com>
2023-03-30 16:09:56 -0300

    atualizando atividades

---
+++ Tecnica de Programação/_ListaDeExercicio01/Exercicio03/src/Normal.java
@@ -0,0 +1,10 @@
+public class Normal extends Ingresso {
+    
+    public Normal(Double valor) {
+        super(valor);
+    }
+
+    public void imprimeIngressoNormal() {
+        System.out.println(getValor());
+    }
+}
---
+++ Tecnica de Programação/_ListaDeExercicio01/Exercicio05/src/Equipamento.java
@@ -0,0 +1,18 @@
+public class Equipamento {
+    boolean ligado;
+
+    public Equipamento() {
+
+    }
+   
+    public boolean isLigado() {return ligado;}
+    public void setLigado(boolean lig) {this.ligado = lig;}
+    
+    public void liga(){
+        ligado = true;
+    }
+    public void desliga(){
+        ligado = false;
+    }
+
+}
